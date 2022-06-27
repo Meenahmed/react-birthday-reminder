b3c609878b7ef389446e338a1988ea62f9cf1ed4
Amina Ahmed <adewuyiamina@gmail.com>
2022-06-27 10:24:01 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+	{
+		id: 1,
+		name: 'Beatrice John',
+		age: 32,
+		image:
+			'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg',
+	},
+	{
+		id: 2,
+		name: 'Peter Smith',
+		age: 30,
+		image:
+			'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg',
+	},
+	{
+		id: 3,
+		name: 'Maria Mathew',
+		age: 28,
+		image:
+			'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?cs=srgb&dl=pexels-christina-morillo-1181519.jpg&fm=jpg',
+	},
+	{
+		id: 4,
+		name: 'Mike Love',
+		age: 2,
+		image:
+			'https://images.pexels.com/photos/35537/child-children-girl-happy.jpg?cs=srgb&dl=pexels-bess-hamiti-35537.jpg&fm=jpg',
+	},
+	{
+		id: 5,
+		name: 'Betty Cute',
+		age: 3,
+		image:
+			'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?cs=srgb&dl=pexels-pixabay-36029.jpg&fm=jpg',
+	},
+];
