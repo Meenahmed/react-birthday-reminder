34b7fa7182275cd059f94e26ba112a6fabfa1767
Amina Ahmed <adewuyiamina@gmail.com>
2022-06-27 12:40:02 -0600

    added remove funtionalities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from './data';
 import List from './List';
 function App() {
 	const [people, setPeople] = useState(data);
+
+	const removePerson = (id) => {
+		const newPeople = people.filter((person) => person.id !== id);
+		setPeople(newPeople);
+	};
 	return (
 		<main>
 			<section className='container'>
 				<h3>{people.length} birthdays coming up</h3>
-				<List people={people} />
-				<button onClick={() => setPeople([])}>clear all</button>
+				<List people={people} removePerson={removePerson} />
+				<button className='btn' onClick={() => setPeople([])}>
+					{people.length === 0 ? 'no upcoming birthday' : 'clear all'}
+				</button>
 			</section>
 		</main>
 	);
--- src/List.js
+++ src/List.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
 	return (
 		<>
 			{people.map((person) => {
 				const { id, name, age, image } = person;
 				return (
 					<article key={id} className='person'>
-						<img src={image} />
-						<div>
-							<h4>{name}</h4>
-							<p>{age}</p>
+						<div className='image-con'>
+							<img src={image} />
+						</div>
+						<div className='info'>
+							<div>
+								<h4>{name}</h4>
+								<p>{age}</p>
+							</div>
+							<button
+								className='btn remove-btn'
+								onClick={() => removePerson(id)}
+							>
+								remove
+							</button>
 						</div>
 					</article>
 				);
